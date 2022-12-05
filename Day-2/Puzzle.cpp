122e0ee31693e5257e180903478bc18e2b231ae3
Phil Pinsdorf <philemon.pinsdorf@live.de>
2022-12-05 23:43:44 +0100

    More performant Code

--- Day-2/Puzzle.cpp
+++ Day-2/Puzzle.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <time.h>
 
 inline int modulo(int a, int b) {
   const int result = a % b;
@@ -8,7 +7,6 @@ inline int modulo(int a, int b) {
 }
 
 int main() {
-    clock_t time = clock();
     std::ifstream file("./input.txt");
 
     int points_1 = 0;
@@ -21,13 +19,7 @@ int main() {
 
         // Part 1
         points_1 += x_int + 1;
-        int win_detection =  x_int - a_int;
-
-        if(win_detection == 0) {
-            points_1 += 3;
-        } else if (win_detection == 1 || win_detection == -2) {
-            points_1 += 6;
-        }
+        points_1 += 6 - 3 * modulo(a_int - x_int - 2, 3);
 
         // Part 2
         points_2 += modulo(a_int + x_int - 1, 3) + 1;
@@ -36,7 +28,6 @@ int main() {
 
     std::cout << "Star One: " << points_1 << std::endl;
     std::cout << "Star Two: " << points_2 << std::endl;
-    std::cout << (float) time / CLOCKS_PER_SEC << " Sekunden" << std::endl;
 
     return 0;
 }
